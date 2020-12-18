71ac6c5a85f5f0763d33958073b751129567f3f6
Santanu Dhali <santanud@in.kovair.com>
2020-12-18 13:20:04 +0530

    Add project files.

---
+++ hello_world/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hello_world
+{
+    public partial class Hello_World : Form
+    {
+        public Hello_World()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("hello World");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
